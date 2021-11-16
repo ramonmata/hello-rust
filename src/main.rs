1794f48d98e2fabadea36d86e351b194c7c38f9f
Ramón Mata <ramonmata@users.noreply.github.com>
2021-11-16 15:36:57 -0600

    Adds code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![allow(unused)]
+
+const ZERO: usize = 0;
+
 fn main() {
-    println!("Hello World\n");
+    println!("The value of constant ZERO is: {}", ZERO);
+
+    const ONE: usize = 1;
+    println!("The value of constant ONE is: {}", ONE);
+
+    let mut x = 5;
+    println!("The value of  is: {}", x);
+    
+    x += 1;
+
+    { // Inner Scope
+        let x:i64 = x * 2;
+        println!("The value of shadow x at inner scope is: {}", x);
+    }
+
+    println!("The value of  is: {}", x);
+
 }
