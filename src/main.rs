d23273e80234a9b047f55deafa65951b2e006d2a
Ramón Mata <ramonmata@users.noreply.github.com>
2021-11-18 12:07:24 -0600

    Add code samples

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 #![allow(unused)]
 fn main() {
-    println!("Hello World\n");
+    let config_max = Some(3u8);
+
+    // Using Match is a bit verbose for a Some value
+    match config_max {
+        Some(max) => println!("Max config is {}", max),
+        _ => ()
+    }
+
+    // Use if let
+    if let Some(max) = config_max {
+        println!("Max config is {}", max);
+    }
+
+    // if let also can use an else like match _
+    if let Some(max) = config_max {
+        println!("Max config is {}", max);
+    } else {
+        println!("No value in max config");
+    }
+
 }
