4085a3249c9e3400b3f57ae6ff2d99cd01201c62
Ramón Mata <ramonmata@users.noreply.github.com>
2021-11-24 13:41:00 -0600

    Accepts command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 #![allow(unused)]
+
+use std::env;
+
 fn main() {
-    println!("Hello World\n");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
