a05ba1d9c5ab1175190c1d42c51ed761d912d9c6
Ramón Mata <ramonmata@users.noreply.github.com>
2021-11-24 14:21:42 -0600

    Refactor to improve modularity

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-#![allow(unused)]
+// #![allow(unused)]
 
 use std::env;
-use std::fs;
+use std::process;
+
+use hello_rust::{Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With Text:\n{}", contents);
+    if let Err(e) = hello_rust::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
